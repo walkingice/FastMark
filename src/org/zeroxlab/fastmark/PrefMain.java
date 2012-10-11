7b69e778dab2aa3569a9a2be1dbbdf6772a0a3ff
Julian_Chu <walkingice@0xlab.org>
2012-10-12 03:39:22 +0800

    be able to reset base url

--- src/org/zeroxlab/fastmark/PrefMain.java
+++ src/org/zeroxlab/fastmark/PrefMain.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -15,18 +16,36 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-
 public class PrefMain extends PreferenceActivity {
+
+    public final static String KEY_BASE_URL = "key_base_url";
+    public final static String KEY_RESET_URL = "key_reset_url";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_main);
+        Preference resetPref = findPreference(KEY_RESET_URL);
+
+        OnPreferenceClickListener listener = new PrefListener();
+        resetPref.setOnPreferenceClickListener(listener);
+    }
+
+    private void resetBaseUrl() {
+        Preference urlPref = findPreference(KEY_BASE_URL);
+        SharedPreferences.Editor editor = urlPref.getEditor();
+        editor.clear();
+        editor.commit();
+        Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show();
+    }
+
+    class PrefListener implements OnPreferenceClickListener {
+        public boolean onPreferenceClick(Preference preference) {
+            if (preference.getKey().equals(KEY_RESET_URL)) {
+                resetBaseUrl();
+            }
+
+            return true;
+        }
     }
 }
